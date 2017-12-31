0bc647aa0d69beb269e780ec3d015548760add0c
Chirag Jain <jainchg@gmail.com>
2017-12-31 16:14:06 +0530

    Updated flask files

--- Testing/flaskcheck.py
+++ Testing/flaskcheck.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flaskrunner import *
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
-    return "Hello Worl!"
+    return render_template('index.html')
+    #return "Hello World!"
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	text = request.form['text']
+	check(text)
+	return text
 
 if __name__ == '__main__':
     app.run(host='192.168.43.195', debug="True")
+    # either leave host empty to use localhost or
+    # use the IPV4 address of your laptop
---
+++ Testing/flaskrunner.py
@@ -0,0 +1,2 @@
+def check(c):
+	print c
