28b04616846a7495d2360c3588e446b7dff33d05
Chirag Jain <jainchg@gmail.com>
2017-12-31 17:26:15 +0530

    Issue #1 Solved. Now working

--- Final/btcheck.py
+++ Final/btcheck.py
@@ -1,9 +1,8 @@
 import bluetooth  # from PyBluez
 
-#sock = 0
-
 def starting():
 	print "Searching for devices..."
+	print "Ensure that the required bluetooth device is paired"
 
 	nearby_devices = bluetooth.discover_devices()
 
@@ -16,39 +15,29 @@ def starting():
 	# bluetooth module must be paired
 
 	selection = input("> ") - 1
-	print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
+	# print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
 	bd_addr = nearby_devices[selection]
 
 	port = 1
-	print "hi1"
 	sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 	sock.connect((bd_addr, port))
+	print "Connected"
 	return sock
 
 #Create the GUI
 def application(check, s):
 	# sock=starting()
 	sock = s
-#Create a connection to the socket for Bluetooth communication
-	print "hi3"
 	if(check=="disconnect"):
     	#Close socket connection to device
 		sock.close()
         
 	if(check=="on"):
-    	#Send 'H' which the Arduino
-    	#detects as turning the light on
+    	#Send 'on' which the Arduino detects as turning the light on
 		data = "1"
 		sock.send(data)
 
 	if(check=="off"):
-    	#Send 'L' to turn off the light
-    	#attached to the Arduino
+    	#Send 'off' to turn off the light attached to the Arduino
 		data = "0"
 		sock.send(data)
-
-#Begin the GUI processing
-# check="off"
-# while(check!="disconnect"):
-# 	application(check)
-# 	check = raw_input()
--- Final/final.py
+++ Final/final.py
@@ -1,46 +1,6 @@
 from flask import Flask, request, render_template
-# import bluetooth
+
 import btcheck as bt
-
-# def btconnect():
-# 	print "Searching for devices..."
-
-# 	nearby_devices = bluetooth.discover_devices()
-
-# 	num = 0
-# 	print "Select your device by entering its coresponding number..."
-# 	for i in nearby_devices:
-# 		num+=1
-# 		print num , ": " , bluetooth.lookup_name( i )
-
-# 	# bluetooth module must be paired
-
-# 	selection = input("> ") - 1
-# 	print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
-# 	bd_addr = nearby_devices[selection]
-
-# 	port = 1
-# 	print "hi1"
-# 	sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-# 	sock.connect((bd_addr, port))
-
-# def runner(inp):
-# 	if(check=="disconnect"):
-#     	#Close socket connection to device
-# 		sock.close()
-        
-# 	if(check=="on"):
-#     	#Send 'H' which the Arduino
-#     	#detects as turning the light on
-# 		data = "1"
-# 		sock.send(data)
-
-# 	if(check=="off"):
-#     	#Send 'L' to turn off the light
-#     	#attached to the Arduino
-# 		data = "0"
-# 		sock.send(data)
-
 
 app = Flask(__name__)
 
@@ -49,7 +9,6 @@ sock = 0
 @app.route('/')
 def hello():
 	return render_template('index.html')
-	#return "Hello World!"
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -60,6 +19,7 @@ def my_form_post():
 
 if __name__ == '__main__':
 	sock = bt.starting()
-	app.run(host='192.168.43.195', debug="True", port=1111)
+	app.run(host='192.168.43.195', port=1111) 
     # either leave host empty to use localhost or
     # use the IPV4 address of your laptop
+    # you may use your own port too
